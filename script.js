8eef56742e508167b87714073fd2ee5b38cbd13c
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-18 23:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-            // Function to write the message to the page
-            function writeMessage(message) {
-                var messageDiv = document.getElementById("message");
-                messageDiv.textContent = message;
-            }
-
-            // Write the message when the DOM is loaded
-            writeMessage("DOM load success");
-        });
+document.addEventListener("DOMContentLoaded",() =>{
+	document.body.innerText=("DOM load success")
+});
